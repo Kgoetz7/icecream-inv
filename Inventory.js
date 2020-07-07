5f19d2dba8d5a703f3949c2179d135c2ce9ed1e1
zksmith <zahiersmith96@gmail.com>
2020-07-06 23:42:09 -0400

    test

--- Inventory.js
+++ Inventory.js
@@ -73,7 +73,7 @@ export default function Inventory({ setPage }) {
           />
         </View>
       </View>
-      <Button title='Adddddddddddd' />
+      <Button title='Add' />
     </SafeAreaView>
   );
 }
